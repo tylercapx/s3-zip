5ec6ddfc821c55a0c584d09e7accf97666ce9e13
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-03-19 17:27:09 +0100

    linted test file

--- test/test_s3_zip.js
+++ test/test_s3_zip.js
@@ -3,8 +3,6 @@ var t = require('tap');
 var fs = require('fs');
 var Stream = require('stream');
 var concat = require('concat-stream');
-
-var fs = require('fs');
 var yauzl = require('yauzl');
 
 var fileStream = function (file, forceError) {
@@ -12,20 +10,21 @@ var fileStream = function (file, forceError) {
   rs.readable = true;
   var fileStream = fs.createReadStream(__dirname + file);
   fileStream
-    .pipe(concat( function buffersEmit(buffer) {
-          if (forceError) {
-            console.log('send end to finalize archive');
-            rs.emit('end');
-          } else {
-            rs.emit('data', { data: buffer, path: file });
-          }
-        })
-      );
+    .pipe(concat(
+      function buffersEmit (buffer) {
+        if (forceError) {
+          console.log('send end to finalize archive');
+          rs.emit('end');
+        } else {
+          rs.emit('data', { data: buffer, path: file });
+        }
+      })
+    );
   fileStream
     .on('end', function () {
-        console.log('end fileStream');
-        rs.emit('end');
-      });
+      console.log('end fileStream');
+      rs.emit('end');
+    });
   return rs;
 };
 
@@ -48,18 +47,18 @@ t.test('test archiveStream and zip file', function (child) {
     .pipe(output);
   archive.on('close', function () {
     console.log('+++++++++++');
-    yauzl.open(__dirname + '/test.zip', function(err, zip) {
+    yauzl.open(__dirname + '/test.zip', function (err, zip) {
       if (err) console.log('err', err);
-        zip.on('entry', function(entry) {
-          // console.log(entry);
-          child.same(entry.fileName, 'fixtures/file.txt');
-          child.same(entry.compressedSize, 11);
-          child.same(entry.uncompressedSize, 20);
-        });
+      zip.on('entry', function (entry) {
+        // console.log(entry);
+        child.same(entry.fileName, 'fixtures/file.txt');
+        child.same(entry.compressedSize, 11);
+        child.same(entry.uncompressedSize, 20);
+      });
 
-        zip.on('close', function() {
-          child.end();
-        });
+      zip.on('close', function () {
+        child.end();
+      });
     });
   });
   child.type(archive, 'object');
@@ -79,14 +78,10 @@ t.test('test archiveStream error', function (child) {
 
 t.test('test archive', function (child) {
   var archive = s3Zip
-    .archive({ 
-        region: 'region', 
-        bucket: 'bucket'
-      }, 
-      'folder', 
-      [file1]
+    .archive({ region: 'region', bucket: 'bucket'},
+     'folder',
+     [file1]
     );
   child.type(archive, 'object');
   child.end();
 });
-
