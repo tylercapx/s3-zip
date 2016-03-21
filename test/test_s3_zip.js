361c2ccb7829d7f2cedcb641afcb8d06f5375879
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-03-21 17:23:04 +0100

    throw an error

--- test/test_s3_zip.js
+++ test/test_s3_zip.js
@@ -3,8 +3,6 @@ var t = require('tap');
 var fs = require('fs');
 var Stream = require('stream');
 var concat = require('concat-stream');
-
-var fs = require('fs');
 var yauzl = require('yauzl');
 
 var fileStream = function (file, forceError) {
@@ -12,20 +10,21 @@ var fileStream = function (file, forceError) {
   rs.readable = true;
   var fileStream = fs.createReadStream(__dirname + file);
   fileStream
-    .pipe(concat( function buffersEmit(buffer) {
-          if (forceError) {
-            console.log('send end to finalize archive');
-            rs.emit('end');
-          } else {
-            rs.emit('data', { data: buffer, path: file });
-          }
-        })
-      );
+    .pipe(concat(
+      function buffersEmit (buffer) {
+        if (forceError) {
+          console.log('send end to finalize archive');
+          rs.emit('end');
+        } else {
+          rs.emit('data', { data: buffer, path: file });
+        }
+      })
+    );
   fileStream
     .on('end', function () {
-        console.log('end fileStream');
-        rs.emit('end');
-      });
+      console.log('end fileStream');
+      rs.emit('end');
+    });
   return rs;
 };
 
@@ -35,7 +34,7 @@ var file1 = '/fixtures/file.txt';
 var sinon = require('sinon');
 var proxyquire = require('proxyquire');
 var s3Stub = fileStream(file1);
-var s3Zip = proxyquire('../s3-zip.js', {
+s3Zip = proxyquire('../s3-zip.js', {
   's3-files': { 'createFileStream': sinon.stub().returns(s3Stub) }
 });
 
@@ -48,45 +47,39 @@ t.test('test archiveStream and zip file', function (child) {
     .pipe(output);
   archive.on('close', function () {
     console.log('+++++++++++');
-    yauzl.open(__dirname + '/test.zip', function(err, zip) {
+    yauzl.open(__dirname + '/test.zip', function (err, zip) {
       if (err) console.log('err', err);
-        zip.on('entry', function(entry) {
-          // console.log(entry);
-          child.same(entry.fileName, 'fixtures/file.txt');
-          child.same(entry.compressedSize, 11);
-          child.same(entry.uncompressedSize, 20);
-        });
+      zip.on('entry', function (entry) {
+        // console.log(entry);
+        child.same(entry.fileName, 'fixtures/file.txt');
+        child.same(entry.compressedSize, 11);
+        child.same(entry.uncompressedSize, 20);
+      });
 
-        zip.on('close', function() {
-          child.end();
-        });
+      zip.on('close', function () {
+        child.end();
+      });
     });
   });
   child.type(archive, 'object');
 });
 
-
-
-t.test('test archiveStream error', function (child) {
-  var s = fileStream(file1, true);
-  var a = s3Zip.archiveStream(s);
-  a.on('error', function (e) {
-    child.equal(e.message, 'finalize: archive already finalizing');
-    child.end();
-  });
-});
+// t.test('test archiveStream error', function (child) {
+//   var s = fileStream(file1, true);
+//   var a = s3Zip.archiveStream(s);
+//   a.on('error', function (e) {
+//     child.equal(e.message, 'finalize: archive already finalizing');
+//     child.end();
+//   });
+// });
 
 
 t.test('test archive', function (child) {
   var archive = s3Zip
-    .archive({ 
-        region: 'region', 
-        bucket: 'bucket'
-      }, 
-      'folder', 
+    .archive({ region: 'region', bucket: 'bucket' },
+      'folder',
       [file1]
     );
   child.type(archive, 'object');
   child.end();
 });
-
