0a733fc261f55bbda99f16d269f5125614c1a7ea
Dan Newman <dnewman@polar3d.com>
2016-11-24 19:43:31 -0800

    Allow renaming the files going into the zip archive

--- s3-zip.js
+++ s3-zip.js
@@ -4,22 +4,22 @@ var archiver = require('archiver');
 
 module.exports = s3Zip = {};
 
-s3Zip.archive = function (opts, folder, files) {
+s3Zip.archive = function (opts, folder, files_s3, files_zip) {
   var self = this;
   var keyStream = s3Files
     .connect({
       region: opts.region,
       bucket: opts.bucket
     })
-    .createKeyStream(folder, files);
+    .createKeyStream(folder, files_s3);
 
   var fileStream = s3Files.createFileStream(keyStream);
-  var archive = self.archiveStream(fileStream);
+  var archive = self.archiveStream(fileStream, files_s3, files_zip);
   return archive;
 };
 
 
-s3Zip.archiveStream = function (stream) {
+s3Zip.archiveStream = function (stream, files_s3, files_zip) {
   var archive = archiver('zip');
   archive.on('error', function (err) {
     console.log('archive error', err);
@@ -32,8 +32,18 @@ s3Zip.archiveStream = function (stream) {
        console.log('don\'t append to zip', file.path);
        return;
      }
-     console.log('append to zip', file.path);
-     archive.append(file.data, { name: file.path });
+     var fname;
+     if (files_zip) {
+       // Place files_s3[i] into the archive as files_zip[i]
+       var i = files_s3.indexOf(file.path);
+       fname = (i >= 0 && i < files_zip.length) ? files_zip[i] : file.path;
+     }
+     else {
+       // Just use the S3 file name
+       fname = file.path;
+     }
+     console.log('append to zip', fname);
+     archive.append(file.data, { name: fname });
    })
    .on('end', function () {
      console.log('end -> finalize');
