f4ad21f7066aa326be7493c441e76a6b14a62b3c
Margaret Sy <margaretsy1016@gmail.com>
2017-02-15 16:22:53 -0800

    add test for empty file

--- test/test_s3_zip.js
+++ test/test_s3_zip.js
@@ -30,6 +30,7 @@ var fileStream = function (file, forceError) {
 }
 
 var file1 = '/fixtures/file.txt'
+var emptyFile = '/fixtures/empty.txt'
 // Stub: var fileStream = s3Files.createFileStream(keyStream);
 var sinon = require('sinon')
 var proxyquire = require('proxyquire')
@@ -71,4 +72,29 @@ t.test('test archive', function (child) {
     )
   child.type(archive, 'object')
   child.end()
+})
+
+t.test('test archive on empty file', function (child) {
+  var output = fs.createWriteStream(join(__dirname, '/test.zip'))
+  var s = fileStream(emptyFile)
+  var archive = s3Zip
+    .archiveStream(s)
+    .pipe(output)
+  archive.on('close', function () {
+    console.log('+++++++++++')
+    yauzl.open(join(__dirname, '/test.zip'), function (err, zip) {
+      if (err) console.log('err', err)
+      zip.on('entry', function (entry) {
+        // console.log(entry);
+        child.same(entry.fileName, 'fixtures/empty.txt')
+        child.same(entry.compressedSize, 0)
+        child.same(entry.uncompressedSize, 0)
+      })
+
+      zip.on('close', function () {
+        child.end()
+      })
+    })
+  })
+  child.type(archive, 'object')
 })
